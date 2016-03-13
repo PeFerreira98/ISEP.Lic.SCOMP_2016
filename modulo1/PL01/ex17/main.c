74e54f59201e9266b6d096d191c6dec92693be1d
PeFerreira98 <1140953@isep.ipp.pt>
2016-03-13 23:43:25 +0000

    Resolução ex17 (complete)

---
+++ modulo1/PL01/ex17/main.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+void readStr(char *inp);
+int divideStr(char *inp, char **arg);
+void sistema(char *impt);
+
+int main(int argc, char **argv){
+	
+	char impt[100];
+	
+	while(1){
+		readStr(impt);
+		
+		if(strcmp("sair", impt) == 0){
+			printf("BYE-BYE!!\n");
+			break;
+		}
+		
+		sistema(impt);
+	}
+	
+	return 0;
+}
+
+void readStr(char *inp){
+	printf("~/SHELL$ ");
+	
+	fgets(inp, 100, stdin);
+	
+	//retira o char a mais no final da string 
+	*(inp+(strlen(inp)-1)) = '\0';
+}
+
+int divideStr(char *inp, char **arg){
+	int i=0;
+	char *temp;
+	
+	temp = strtok (inp," ");
+	*(arg+i) = temp;
+	
+	while (temp != NULL)
+	{	
+		*(arg+i) = temp;
+		i++;
+		
+		temp = strtok (NULL, " ");
+		//Alternativelly, a null pointer may be specified, in which case the 
+		//function continues scanning where a previous successful call to the function ended.
+	}
+	
+	return i;
+}
+
+void sistema(char *impt){
+	char **argum;
+	int i, lineSize, estado;
+	pid_t p;
+	
+	//alocação de memoria
+	argum = (char**) malloc (10*sizeof(char*));
+	for (i = 0; i < 10; i++)
+	{
+		*(argum+i) = (char*) malloc (10*sizeof(char));
+	}
+	
+	//dividir string instrução em varias partes
+	lineSize = divideStr(impt, argum);
+	argum[lineSize] = NULL;
+	
+	//execução da instrução c/ fork
+	p = fork();
+	
+	if (p == 0)
+	{
+		execvp(argum[0], argum);
+	}
+	
+	waitpid(p, &estado, 0);
+}
