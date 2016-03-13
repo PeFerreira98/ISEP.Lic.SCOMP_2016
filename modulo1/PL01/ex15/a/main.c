8f299704ef5520c6623fc026c0f37aa04b6e1b79
Mark956 <1140956@isep.ipp.pt>
2016-03-13 17:57:19 +0000

    Resolução ex15a

---
+++ modulo1/PL01/ex15/a/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[]){
+	
+	pid_t p[argc-1];
+	int ret, i = 1, estado;
+	
+	while(i < argc){
+		
+		p[i-1] = fork();
+		
+		if(p[i-1] == 0){
+			ret = execlp (argv[i], argv[i], NULL);
+			printf("%s : é um comando inválido\n", argv[i]);
+			exit(0);
+		}
+		i++;
+	}
+	
+	for(i = 0; i < argc-1; i++){
+		waitpid(p[i], &estado, 0);
+	}
+	
+	return 0;
+}
