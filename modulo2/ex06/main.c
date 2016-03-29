e7d376e6cff50d8ccbddff54702eddd722472cfa
Mark956 <1140956@isep.ipp.pt>
2016-03-29 14:37:06 +0100

    Resolução parcial ex06

--- modulo2/ex06/main.c
+++ modulo2/ex06/main.c
@@ -6,19 +6,26 @@
 #include <unistd.h>
 
 #define BUFFER_SIZE 100
-#define NUM_ELEM 1000
+#define NUM_ELEM 10
 #define NUM_PROC 5
 
 int main(void){
 	
 	pid_t p[NUM_PROC];
-	int fd[2], vec1[NUM_ELEM], vec2[NUM_ELEM], i, j, aux, tot=0, estado;
+	int matrix[NUM_PROC][2];
+	int fd[2];
+	int vec1[NUM_ELEM], vec2[NUM_ELEM], i, j, aux, tot=0, estado;
 	
 	for(i = 0; i < NUM_ELEM; i++){//Preencher vectores
 		vec1[i] = 1;
-		vec2[i] = 2;
+		vec2[i] = 1;
 	}
-	pipe(fd);
+	for(i = 0; i < NUM_PROC; i++){
+		matrix+i = fd;
+	}
+	for(i = 0; i < NUM_PROC; i++){
+		pipe(*(matrix+i));
+	}
 	
 	for(i = 0; i < NUM_PROC; i++){
 		
@@ -29,14 +36,13 @@ int main(void){
 			for(j = ((NUM_ELEM/NUM_PROC)*i); j < ((NUM_ELEM/NUM_PROC)*i +(NUM_ELEM/NUM_PROC)); j++){
 				total += vec1[j] + vec2[j];
 			}
-			write(fd[1], &total, sizeof(int));
-			close(fd[1]);
+			write(matrix+i+1, &total, sizeof(int));
+			close(matrix+i+1);
 			exit(0);				
 		}
-		read(fd[0],&aux,sizeof(int));
+		read(matrix+i,&aux,sizeof(int));		
 		tot = tot + aux;
 	}
-	close(fd[0]);
 	
 	for(i = 0; i < NUM_PROC; i++){
 		waitpid(p[i], &estado, 0);
