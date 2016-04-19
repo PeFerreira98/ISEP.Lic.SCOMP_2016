cf998d5b23ab248443d5ef1bfab910b400a65a9c
PeFerreira98 <1140953@isep.ipp.pt>
2016-04-19 15:37:27 +0100

    Add fprintf to ex01

--- modulo4/ex01/main.c
+++ modulo4/ex01/main.c
@@ -13,30 +13,49 @@
 #define NUM_ELEM 200
 
 int main(){
+	FILE *fp;
 	pid_t p[NUM_PROC];
-	int i, j, estado;
+	int i, j, c, estado;
 	sem_t *sem;
 	
+	//Semaphore & File Management
+	fp = fopen("file.txt", "w+");
 	sem = sem_open("semaforo", O_CREAT | O_EXCL, 0644, 1);
 	
 	for(i = 0; i < NUM_PROC; i++){
 		
 		p[i] = fork();
-		
 		if(p[i] ==0){
+			
 			sem_wait(sem);
-			for( j = NUM_ELEM*i; j<NUM_ELEM*(i+1);j++){
-				printf("%d\n", j);
+			for( j = NUM_ELEM*i; j<NUM_ELEM*(i+1); j++){
+				fprintf(fp, "%d ", j);
 			}
-			printf("----\n");
+			
+			fprintf(fp, "----\n");
+			
 			sem_post(sem);
 			exit(0);
 		}
 	}
-	for(i = 0; i < NUM_PROC; i++){
-		waitpid(p[i], &estado, 0);
+	
+	for(i = 0; i < NUM_PROC; i++) waitpid(p[i], &estado, 0);
+	
+	fclose(fp);
+	
+	//Leitura do ficheiro
+	fp = fopen("file.txt", "r");
+	while(1){
+		
+		c = fgetc(fp);
+		if(feof(fp)) break; //Sai do ciclo quando EOF
+		
+		printf("%c", c);
 	}
-
-	sem_unlink("semaforo");
+	fclose(fp);
+	
+	//Semaphore Management
+	if(sem_unlink("semaforo") != 0) printf("Error! >> sem_unlink \n");
+	
 	return 0;
 }
