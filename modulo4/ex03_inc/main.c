1d5ef857e09282cb3f43f3627862bdf2f80da7b4
PeFerreira98 <1140953@isep.ipp.pt>
2016-04-20 16:30:56 +0100

    Resolução ex03 (inc.) verf. multiple execs

---
+++ modulo4/ex03_inc/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <semaphore.h>
+
+#define NUM_PROC 5
+#define NUM_ELEM 200
+
+int main(){
+	FILE *fp;
+	pid_t p[NUM_PROC];
+	int i, estado;
+	sem_t *sem;
+
+	//Semaphore Management
+	sem = sem_open("semaforo", O_CREAT | O_EXCL, 0644, 1);
+	
+	for(i = 0; i < NUM_PROC; i++){
+		
+		p[i] = fork();
+		if(p[i] == 0){
+			
+			sem_wait(sem);
+			
+			fp = fopen("file.txt", "a"); //a >> Append
+			fprintf(fp, "Eu sou o processo com o PID %d\n", getpid());
+			fclose(fp);
+			
+			printf("Wait 2 sec...\n");
+			sleep(2);
+			sem_post(sem);
+			
+			exit(0);
+		}
+	}
+	
+	for(i = 0; i < NUM_PROC; i++) waitpid(p[i], &estado, 0);
+
+	//Semaphore Management
+	if(sem_unlink("semaforo") != 0) printf("Error! >> sem_unlink \n");
+	
+	return 0;
+}
